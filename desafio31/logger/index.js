512a31846a32e818c6ecc8b917328d71fe1a4750
Felipe Lerda <lerdafelipe@gmail.com>
2021-11-11 21:00:46 -0300

    ok

--- desafio31/logger/index.js
+++ desafio31/logger/index.js
@@ -5,6 +5,8 @@ const logger = winston.createLogger({
     level= 'info',
     transports: [
         new winston.transports.Console({ level: 'info' }),
+        new winston.transports.Console({ level: 'error' }),
+        new winston.transports.Console({ level: 'warn' }),
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
         new winston.transports.File({ filename: 'warn.log', level: 'warn' })
     ]
